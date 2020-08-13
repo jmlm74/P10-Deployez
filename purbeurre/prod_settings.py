34bf3f38974c0381b41a307746e450b574c7f92e
jmlm74 <jmlm74@gmail.com>
2020-08-13 20:43:14 +0200

    init project

---
+++ purbeurre/prod_settings.py
@@ -0,0 +1,25 @@
+import os
+import dj_database_url
+
+from .settings import *
+
+
+# debug
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+# disable django debug toolbar
+INTERNAL_IPS = []
+
+# security
+ALLOWED_HOSTS = ['purbeurre-jmlm74.herokuapp.com', ]
+SECRET_KEY = get_env_variable('SECRET_KEY', '')
+
+# postgres database
+DATABASES['default'] = dj_database_url.config()
+
+# static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# white noise for static files
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
